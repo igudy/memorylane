5803730ab1e22d56214ccaf24bf631dc62b65b5a
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-08 15:05:17 +0100

    Client first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h2>Web App</h2>
+    </>
+  )
+}
+
+export default App
--- server/models/User.js
+++ server/models/User.js
@@ -41,4 +41,3 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 export default User;
-
