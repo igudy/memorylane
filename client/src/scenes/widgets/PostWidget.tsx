7ff11d1721a5281c250525950e15838390e12a63
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-19 14:03:58 +0100

    Friend List Widget added

--- client/src/scenes/widgets/PostWidget.tsx
+++ client/src/scenes/widgets/PostWidget.tsx
@@ -12,6 +12,19 @@ import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setPost } from "../../state"
 
+// Like function
+const patchLike = async () => {
+  const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+    method: "PATCH",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userId: loggedInUserId }),
+  })
+  const updatedPost = await response.json()
+  dispatch(setPost({ post: updatedPost }))
+}
 const PostWidget = ({
   postId,
   postUserId,
--- client/src/scenes/widgets/PostsWidget.tsx
+++ client/src/scenes/widgets/PostsWidget.tsx
@@ -1,37 +1,41 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setPosts } from "../../state"
+import PostWidget from "./PostWidget"
 
-const PostsWidget = ({ userId, isProfile }) => {
+const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch()
   const posts = useSelector((state) => state.posts)
   const token = useSelector((state) => state.token)
 
   const getPosts = async () => {
-    const response = await fetch("http:localhost:3001/posts", {
+    const response = await fetch("http://localhost:3001/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     })
     const data = await response.json()
-    console.log(data)
     dispatch(setPosts({ posts: data }))
   }
 
   const getUserPosts = async () => {
-    const response = await fetch(`http:localhost:3001/posts/${userId}/posts`, {
-      method: "GET",
-      headers: { Authorizaton: `Bearer ${token}` },
-    })
+    const response = await fetch(
+      `http://localhost:3001/posts/${userId}/posts`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
     const data = await response.json()
     dispatch(setPosts({ posts: data }))
   }
+
   useEffect(() => {
     if (isProfile) {
       getUserPosts()
     } else {
       getPosts()
     }
-  }, []) //eslint-disable-line react-hooks/exhaustive-deps
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
@@ -48,7 +52,7 @@ const PostsWidget = ({ userId, isProfile }) => {
           likes,
           comments,
         }) => (
-          <PostsWidget
+          <PostWidget
             key={_id}
             postId={_id}
             postUserId={userId}
