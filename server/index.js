395ff6119f727591a75fec63e8ce12b8171feed3
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-23 18:23:28 +0100

    Text corrected

--- server/index.js
+++ server/index.js
@@ -14,9 +14,6 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -53,7 +50,7 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Nenory Lane API</h1>');
+  res.send('<h1>Memory Lane API</h1>');
 });
 
 /* MONGOOSE SETUP */
