e49d8e8d0020fa0c0cfeece2fbc511d5fab1ef0a
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-23 13:58:28 +0100

    Server update

--- server/index.js
+++ server/index.js
@@ -62,9 +62,6 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
 
