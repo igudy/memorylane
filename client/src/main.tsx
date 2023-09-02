73c328278653539282c6fac1146c7365d292b78f
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-09-02 09:59:51 +0100

    getDefaultMiddleware modified

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,53 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import authReducer from "./state"
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
-import { Provider } from "react-redux"
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import "./index.css"
+// import App from "./App"
+// import authReducer from "./state"
+// import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
+// import { Provider } from "react-redux"
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist"
+// import storage from "redux-persist/lib/storage"
+// import { PersistGate } from "redux-persist/integration/react"
+
+// const persistConfig = { key: "root", storage, version: 1 }
+// const persistedReducer = persistReducer(persistConfig, authReducer)
+// const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// })
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistStore(store)}>
+//         <App />
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// )
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
   persistStore,
   persistReducer,
@@ -14,12 +57,12 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
-} from "redux-persist"
-import storage from "redux-persist/lib/storage"
-import { PersistGate } from "redux-persist/integration/react"
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-const persistConfig = { key: "root", storage, version: 1 }
-const persistedReducer = persistReducer(persistConfig, authReducer)
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
@@ -28,7 +71,7 @@ const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
-})
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -39,4 +82,4 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
